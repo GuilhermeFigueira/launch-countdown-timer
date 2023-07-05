2ea50b1e2d48e1ddfd8cc6be75385b5c89eb3977
Guilherme Figueira <guibf2004@gmail.com>
2023-07-05 11:53:34 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			blue: {
+				grayish: "hsl(237, 18%, 59%)",
+				desaturated: "hsl(236, 21%, 26%)",
+				dark: "hsl(235, 16%, 14%)",
+				black: "hsl(234, 17%, 12%)",
+			},
+			red: "hsl(345, 95%, 68%)",
+		},
+	},
+	plugins: [],
+};
